45131f25c98e4ef167d0eb7ab7941dcf614a6c46
Steven Nance <steven@devtrw.com>
2013-05-13 12:03:53 -0700

    Update file uploader service to use updated vendor lib

--- Services/FileUploader.php
+++ Services/FileUploader.php
@@ -12,7 +12,7 @@ class FileUploader
     }
 
     /**
-     * Get a list of files already present. The 'folder' option is required. 
+     * Get a list of files already present. The 'folder' option is required.
      * If you pass consistent options to this method and handleFileUpload with
      * regard to paths, then you will get consistent results.
      */
@@ -34,11 +34,11 @@ class FileUploader
     /**
      * Sync existing files from one folder to another. The 'fromFolder' and 'toFolder'
      * options are required. As with the 'folder' option elsewhere, these are appended
-     * to the file_base_path for you, missing parent folders are created, etc. If 
+     * to the file_base_path for you, missing parent folders are created, etc. If
      * 'fromFolder' does not exist no error is reported as this is common if no files
      * have been uploaded. If there are files and the sync reports errors an exception
      * is thrown.
-     * 
+     *
      * If you pass consistent options to this method and handleFileUpload with
      * regard to paths, then you will get consistent results.
      */
@@ -53,13 +53,13 @@ class FileUploader
      * example:
      *
      * $id = $this->getRequest()->get('id');
-     * // Validate the id, make sure it's just an integer, validate the user's right to edit that 
+     * // Validate the id, make sure it's just an integer, validate the user's right to edit that
      * // object, then...
      * $this->get('punkave.file_upload').handleFileUpload(array('folder' => 'photos/' . $id))
-     * 
+     *
      * DOES NOT RETURN. The response is generated in native PHP by BlueImp's UploadHandler class.
      *
-     * Note that if %file_uploader.file_path%/$folder already contains files, the user is 
+     * Note that if %file_uploader.file_path%/$folder already contains files, the user is
      * permitted to delete those in addition to uploading more. This is why we use a
      * separate folder for each object's associated files.
      *
@@ -105,47 +105,13 @@ class FileUploader
         @mkdir($uploadDir, 0777, true);
         $upload_handler = new \PunkAve\FileUploaderBundle\BlueImp\UploadHandler(
             array(
-                'upload_dir' => $uploadDir, 
-                'upload_url' => $webPath . '/' . $originals['folder'] . '/', 
+                'upload_dir' => $uploadDir,
+                'upload_url' => $webPath . '/' . $originals['folder'] . '/',
                 'script_url' => $options['request']->getUri(),
                 'image_versions' => $sizes,
                 'accept_file_types' => $allowedExtensionsRegex,
                 'max_number_of_files' => $options['max_number_of_files'],
             ));
-
-        // From https://github.com/blueimp/jQuery-File-Upload/blob/master/server/php/index.php
-        // There's lots of REST fanciness here to support different upload methods, so we're
-        // keeping the blueimp implementation which goes straight to the PHP standard library.
-        // TODO: would be nice to port that code fully to Symfonyspeak.
-
-        header('Pragma: no-cache');
-        header('Cache-Control: no-store, no-cache, must-revalidate');
-        header('Content-Disposition: inline; filename="files.json"');
-        header('X-Content-Type-Options: nosniff');
-        header('Access-Control-Allow-Origin: *');
-        header('Access-Control-Allow-Methods: OPTIONS, HEAD, GET, POST, PUT, DELETE');
-        header('Access-Control-Allow-Headers: X-File-Name, X-File-Type, X-File-Size');
-
-        switch ($_SERVER['REQUEST_METHOD']) {
-            case 'OPTIONS':
-                break;
-            case 'HEAD':
-            case 'GET':
-                $upload_handler->get();
-                break;
-            case 'POST':
-                if (isset($_REQUEST['_method']) && $_REQUEST['_method'] === 'DELETE') {
-                    $upload_handler->delete();
-                } else {
-                    $upload_handler->post();
-                }
-                break;
-            case 'DELETE':
-                $upload_handler->delete();
-                break;
-            default:
-                header('HTTP/1.1 405 Method Not Allowed');
-        }
 
         // Without this Symfony will try to respond; the BlueImp upload handler class already did,
         // so it's time to hush up
